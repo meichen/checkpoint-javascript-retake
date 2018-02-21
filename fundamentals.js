fd5500a3582d8ec0d9dfe5f4ad921a317c337bc1
MEI CHEN <MEICHEN6@GMAIL.COM>
2018-02-21 13:53:46 -0500

    fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -2,30 +2,32 @@
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
-
-
+var colors = ["red", "blue", "yellow"];
 
 // #2: Access the last item in the array and assign to a variable called `lastColor`.
 // Type your solution immediately below this line:
-
-
+var lastColor = colors[2];
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
-
+favoriteColors = [];
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
 
-
+for (var i = 0; i < colors.length; i++);
+favoriteColors[i] = colors[i];
 
 // #5: Create an object literal called `student` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
+var student = {
+  name: "Jack",
+  yearsOfCoding: 3,
+  codingSpeed: "fast"
+};
 
 // #6: Add a `attends-office-hours` (spelled exactly) property to `student` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+student["attends-office-hours"] = true;
